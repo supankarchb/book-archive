833feb68d55a16c94607c00722b8f0c0b32737b7
supankarchb <supankar710@gmail.com>
2021-09-02 05:38:35 +0600

    first commit

---
+++ book.js
@@ -0,0 +1,59 @@
+const searchBook  = () =>{
+
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+
+//Error Handle function
+    const errorShow = () =>{
+      const errorMsg = document.getElementById('error-handle');
+      errorMsg.innerHTML = `<h2 class="d-flex justify-content-center">please input a valid name</h2>`;
+    }
+
+    const noError = () => {
+      const noErrorMsg = document.getElementById('error-handle');
+      noErrorMsg.textContent = '';
+    }
+// Error checking
+    
+    if(searchText === ''){
+      errorShow();
+    }
+
+    else{
+      const url = `http://openlibrary.org/search.json?q=${searchText}`;
+      fetch(url)
+      .then(res => res.json())
+      .then (data => displaySearchResult(data));
+      noError();
+    }
+
+}
+// Show result area
+const displaySearchResult = (docs) =>{
+
+    const searchResult = document.getElementById('book-search-result');
+    searchResult.textContent = '';
+    const booklist = docs.docs;
+
+    booklist.forEach (doc =>{
+      // const arrayLength = doc.length;
+      const howManySearch = document.getElementById('show-search-result');
+      howManySearch.innerHTML = `<h2 class="d-flex justify-content-center">your search result ${doc.length}</h2>`
+
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = ` <div class="card">
+        <img src="http://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Book Name :"${doc.subject}"</h5>
+          <p class="card-text"><small>Author:<b> ${doc.author_name}</b></small></p>
+          <p class="card-text">First Publish : <b>${doc.first_publish_year}</b></p>
+          <p class="card-text">Publisher : <b>${doc.publisher}</b></p>
+         
+        </div>
+        
+      </div>`;
+      searchResult.appendChild(div);
+    })
+}
